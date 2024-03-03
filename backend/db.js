f7d8b0969db89c2eb5aa28eae96f9a0850426db8
Tanzeel <ktanzeel80@gmail.com>
2024-03-03 15:39:11 +0530

    Configuring Backend

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI =
+  "mongodb://localhost:27017?readPreference=primary&appname=MongoDV%20Compass&directConnection=true&ssl=false";
+
+const connectToMongoDB = () => {
+  mongoose.connect(mongoURI);
+  console.log("Connected");
+};
+
+module.exports = connectToMongoDB;
