6966b48c3729478ff909b6465e821650f0da6507
Tanzeel <ktanzeel80@gmail.com>
2024-03-06 19:34:01 +0530

    Adding API calls

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const connectToMongoDB = require("./db");
 const express = require("express");
+const cors = require("cors");
 
 connectToMongoDB();
 const app = express();
 const port = 5000;
 app.use(express.json());
+app.use(cors());
 
 //Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`CloudBooks Backend listening on port http://localhost:${port}`);
 });
